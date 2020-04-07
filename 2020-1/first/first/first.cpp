8887d8f61620309a23dc0c32468a54a61be5504a
jeffan15 <37373382+jeffan15@users.noreply.github.com>
2020-04-07 08:47:55 -0500

    tarea 1

---
+++ 2020-1/first/first/first.cpp
@@ -0,0 +1,38 @@
+// first.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    //declaro las variables
+    int x;
+    int y;
+    int z;
+    int a;
+    int b;
+    //inicio variables
+    std::cout << "ingrese el valor de x:\n";
+    std::cin >> x;
+    std::cout << "ingrese el valor de y:\n";
+    std::cin >> y;
+    z = x + y;
+    a = x - y;
+    b = x * y;
+
+    std::cout << "El resultado de la suma es:" << z <<std::endl;
+    std::cout << "El resultado de la resta es:" << a << std::endl;
+    std::cout << "El resultado de la multiplicacion es:" << b << std::endl;
+
+    return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
